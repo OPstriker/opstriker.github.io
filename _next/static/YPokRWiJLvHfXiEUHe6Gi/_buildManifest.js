4afcf16b9ecd9aa21529b7fcc4823cb0f1ea5d1f
OPstriker <76495154+OPstriker@users.noreply.github.com>
2022-11-24 15:45:26 +0000

    Deploying to gh-pages from @ OPstriker/opstriker.github.io@ab6b4ea8cb8a2617f2162e2f0eff3c5978621da2 🚀

---
+++ _next/static/YPokRWiJLvHfXiEUHe6Gi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-581ba1212affc918.js","static/chunks/pages/index-eb0bdf35856185f5.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
